75fad4330f33affc2edeb8091cb819daec073075
safoorarafiq <safoorahingoro@gmail.com>
2024-06-22 23:10:29 +0500

    Add files via upload

---
+++ banoqabil3.0/assigment2.py
@@ -0,0 +1,21 @@
+#area of triangle
+
+height=5
+base=3
+f=0.5
+area=0.5*height*base
+print("the area of triangle is",area)
+
+#area of square
+
+sideA=9
+sideB=9
+area=sideA*sideB
+print("the area of square is",area)
+
+#area of circle 
+
+π=3.14
+radius=7
+area=π*radius*radius
+print("the area of circle is",area)
