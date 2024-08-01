df8c8a689abcfa6c31576fd95016bf718e66a289
safoorarafiq <safoorahingoro@gmail.com>
2024-08-02 00:49:56 +0500

    upladed a new file

---
+++ banoqabil3.0/assignment6.py
@@ -0,0 +1,49 @@
+#list
+def sum_of_list(lst):
+    total = 0
+    for i in lst:
+        total += i
+    return total
+
+lst = [32, 54, 23, 57, 68]
+print('The sum of the list is:', sum_of_list(lst)) 
+ 
+#tuple
+def smallest_number(tpl):
+    for i in t:
+        small =min(t)
+    return small
+
+t = (5, 3, 2.5, 7, 3, 4.5, 0.5)
+print('The smallest number of tuple is:', smallest_number(t))
+
+#set
+def square_items(numbers_set):
+    squared_set = set()  
+    for i in numbers_set:
+        squared_set.add(i * i)
+    return squared_set
+
+a = {2, 4, 6, 8, 3, 5, 7, 9}
+squared_set = square_items(a)
+print(squared_set)
+
+#Dictionary
+def get_keys(dic):
+    key_lst=[]
+    for key in d:
+       key_lst.append(key)
+    return key_lst    
+d={'student':'Maryum',
+   'percentage':86.34,
+   'grade':'A'} 
+key=get_keys(d)
+print(key)     
+
+        
+        
+
+
+        
+        
+
